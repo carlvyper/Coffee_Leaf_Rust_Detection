132210487f7beb51bc3828596865bc95190466d0
Nimlord mwiti muriuki <nimlordmwiti88@gmail.com>
2026-07-08 22:35:28 +0300

    inject adsense via index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,12 @@
+// Dynamically inject Google AdSense Script
+if (typeof window !== 'undefined') {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = "https://googlesyndication.com";
+  script.crossOrigin = "anonymous";
+  document.head.appendChild(script);
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
